f147bbe118e7c090a5e6955832c5bb71af45a576
Jacqueline Angelin <jacqueline.angelin@yahoo.com.br>
2021-02-10 18:02:41 -0300

    prova Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'cards', component: CardsComponent},
+  {path: 'pricing', component: PricingComponent},
+  {path: 'blocks', component: BlocksComponent},
+  {path: 'forms', component: FormsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PricingComponent,
+    BlocksComponent,
+    CardsComponent,
+    FormsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
